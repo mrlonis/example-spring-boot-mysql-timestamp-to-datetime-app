144fa7b4ecd57be4630fa6afbe6b9bcb6efeec55
Matthew Lonis <mattlonis@gmail.com>
2025-04-06 13:25:51 -0400

    Test against more MySQL Versions

--- src/test/java/com/mrlonis/time/ApplicationTests.java
+++ src/test/java/com/mrlonis/time/ApplicationTests.java
@@ -3,6 +3,7 @@ package com.mrlonis.time;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.mrlonis.time.util.TestcontainersConfigurations;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -11,11 +12,64 @@ import org.springframework.context.annotation.Import;
  * This is a basic context load test to ensure that the application can start up and that the Spring context is able to
  * load
  */
-@Import(TestcontainersConfigurations.TestcontainersConfigurationMySQL8_0.class)
-@SpringBootTest
 class ApplicationTests {
-    @Test
-    void contextLoads() {
-        assertTrue(true);
+    @Nested
+    @Import(TestcontainersConfigurations.TestcontainersConfigurationMySQL5_7.class)
+    @SpringBootTest
+    class ApplicationMySQL5_7Tests {
+        @Test
+        void contextLoads() {
+            assertTrue(true);
+        }
+    }
+
+    @Nested
+    @Import(TestcontainersConfigurations.TestcontainersConfigurationMySQL8_0.class)
+    @SpringBootTest
+    class ApplicationMySQL8_0Tests {
+        @Test
+        void contextLoads() {
+            assertTrue(true);
+        }
+    }
+
+    @Nested
+    @Import(TestcontainersConfigurations.TestcontainersConfigurationMySQL8.class)
+    @SpringBootTest
+    class ApplicationMySQL8Tests {
+        @Test
+        void contextLoads() {
+            assertTrue(true);
+        }
+    }
+
+    @Nested
+    @Import(TestcontainersConfigurations.TestcontainersConfigurationMySQL9.class)
+    @SpringBootTest
+    class ApplicationMySQL9Tests {
+        @Test
+        void contextLoads() {
+            assertTrue(true);
+        }
+    }
+
+    @Nested
+    @Import(TestcontainersConfigurations.TestcontainersConfigurationMySQL_LTS.class)
+    @SpringBootTest
+    class ApplicationMySQLLTSTests {
+        @Test
+        void contextLoads() {
+            assertTrue(true);
+        }
+    }
+
+    @Nested
+    @Import(TestcontainersConfigurations.TestcontainersConfigurationMySQL_Latest.class)
+    @SpringBootTest
+    class ApplicationMySQLLatestTests {
+        @Test
+        void contextLoads() {
+            assertTrue(true);
+        }
     }
 }
--- src/test/java/com/mrlonis/time/util/TestcontainersConfigurations.java
+++ src/test/java/com/mrlonis/time/util/TestcontainersConfigurations.java
@@ -9,11 +9,59 @@ import org.testcontainers.utility.DockerImageName;
 
 public class TestcontainersConfigurations {
     @TestConfiguration(proxyBeanMethods = false)
+    public static class TestcontainersConfigurationMySQL5_7 {
+        @Bean
+        @ServiceConnection
+        MySQLContainer<?> mysqlContainer() {
+            return new MySQLContainer<>(DockerImageName.parse("mysql:5.7"))
+                    .withImagePullPolicy(PullPolicy.alwaysPull());
+        }
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
     public static class TestcontainersConfigurationMySQL8_0 {
         @Bean
         @ServiceConnection
         MySQLContainer<?> mysqlContainer() {
             return new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+                    .withImagePullPolicy(PullPolicy.alwaysPull());
+        }
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    public static class TestcontainersConfigurationMySQL8 {
+        @Bean
+        @ServiceConnection
+        MySQLContainer<?> mysqlContainer() {
+            return new MySQLContainer<>(DockerImageName.parse("mysql:8")).withImagePullPolicy(PullPolicy.alwaysPull());
+        }
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    public static class TestcontainersConfigurationMySQL9 {
+        @Bean
+        @ServiceConnection
+        MySQLContainer<?> mysqlContainer() {
+            return new MySQLContainer<>(DockerImageName.parse("mysql:9")).withImagePullPolicy(PullPolicy.alwaysPull());
+        }
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    public static class TestcontainersConfigurationMySQL_LTS {
+        @Bean
+        @ServiceConnection
+        MySQLContainer<?> mysqlContainer() {
+            return new MySQLContainer<>(DockerImageName.parse("mysql:lts"))
+                    .withImagePullPolicy(PullPolicy.alwaysPull());
+        }
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    public static class TestcontainersConfigurationMySQL_Latest {
+        @Bean
+        @ServiceConnection
+        MySQLContainer<?> mysqlContainer() {
+            return new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
                     .withImagePullPolicy(PullPolicy.alwaysPull());
         }
     }
